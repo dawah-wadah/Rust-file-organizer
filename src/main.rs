bc2642b59500004bf2be9446ef5bedf1c44dd7f1
Wadah Adlan <wadahadlan@gmail.com>
2022-09-15 13:06:51 -0400

    recursively check for files, beginning json serializing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![allow(unused)]
+use readable_byte::readable_byte;
+use std::env;
 use std::error::Error;
-use std::{env};
 use std::fs::{self, Metadata};
-
-
+use std::os::unix::prelude::MetadataExt;
+use std::str::FromStr;
+use fs_extra::dir::get_size;
 // use walkdir::WalkDir;
 
 fn main() {
@@ -18,62 +20,94 @@ fn main() {
     // let not_valid = is_path_redux(&config.path, "game");
     println!("{:?}", args);
     let path_valid_bool = path_valid.unwrap();
-    println!("Is this filepath leads to a directory: {:?}", path_valid_bool);
-    println!("Is this filepath leads to a file: {:?}", file_valid.unwrap());
+    println!(
+        "Is this filepath leads to a directory: {:?}",
+        path_valid_bool
+    );
+    println!(
+        "Is this filepath leads to a file: {:?}",
+        file_valid.unwrap()
+    );
 
     if path_valid_bool {
-        check_for_recent_changes(&config.path);
+        record_file(&config.path);
     }
     return;
 }
 
 struct Config {
     path: String,
-    destination: String
+    destination: String,
+}
+
+struct FileEntry {
+    name: String,
+    old_path: String,
+    new_path: String,
+    date_created: String,
+    date_modified: String,
+    date_moved: String,
 }
 
 impl Config {
     fn new(args: &[String]) -> Result<Config, String> {
-    if args.len() <= 1 {
-        return Err("not enough params".to_string())
-    }
+        if args.len() <= 1 {
+            return Err("not enough params".to_string());
+        }
         let path: String = args[1].clone();
         let destination: String = args[2].clone();
-    
-        return Ok(Config{path, destination})
-    }
 
+        return Ok(Config { path, destination });
+    }
 }
 
 fn is_path_redux(filepath: &str, classifer: &str) -> std::io::Result<bool> {
- let attr = fs::metadata(filepath)?;
+    let attr = fs::metadata(filepath)?;
     // inspect attr ...
     match classifer {
         "directory" => return Ok(attr.is_dir()),
         "file" => return Ok(attr.is_file()),
-        "symlink" =>return Ok(attr.is_symlink()),
-        _ => panic!("not a valid arguement")
+        "symlink" => return Ok(attr.is_symlink()),
+        _ => panic!("not a valid arguement"),
     }
     // inspect attr ...
 }
 
-fn is_path(filepath: &str) -> std::io::Result<bool> {
- let attr = fs::metadata(filepath)?;
-    // inspect attr ...
-    Ok(attr.is_dir())
-    // inspect attr ...
+fn record_file(filepath: &str) -> Result<bool, Box<dyn std::error::Error>> {
+    let current_dir = filepath;
+    for entry in fs::read_dir(current_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        let metadata = fs::metadata(&path)?;
+        let last_modified = metadata.modified()?.elapsed()?.as_secs();
+        let last_accessed = metadata.accessed()?.elapsed()?.as_secs();
+
+        let file_size = readable_byte::b(metadata.len());
+        if metadata.is_file() && false{
+            println!(
+                "Last modified: {:?} seconds, is read only: {:?}, size: {:?}, filename: {:?}",
+                last_modified,
+                metadata.permissions().readonly(),
+                file_size,
+                path.file_name().ok_or("No filename")?,
+            );
+
+            let filename = path.file_name().ok_or("No filename")?;
+        }
+        if metadata.is_dir() {
+            println!(
+                "Folder Path: {:?}, is read only: {:?}, size: {:?}",
+                path.file_name().ok_or("No filename")?,
+                metadata.permissions().readonly(),
+                readable_byte::b(get_size(&path).unwrap()),
+            );
+            record_file(&path.into_os_string().into_string().unwrap());
+        }
+    }
+
+    return Ok(true);
 }
-// fn mime(file_path: &str) {
-//     match mime_type::File::new(Path::new(file_path)).get_mime_type() {
-//         Err(e) => {
-//             eprintln!("{}", e);
-//             std::process::exit(exitcode::DATAERR);
-//         }
-//         Ok(m) => {
-//             println!("{}", m)
-//         }
-//     };
-// }
 
 fn check_for_recent_changes(filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
     let current_dir = filepath;
@@ -96,7 +130,7 @@ fn check_for_recent_changes(filepath: &str) -> Result<(), Box<dyn std::error::Er
                 last_modified,
                 metadata.permissions().readonly(),
                 metadata.len(),
-                path.file_name().ok_or("No filename")?
+                path.file_name().ok_or("No filename")?,
             );
         }
         if last_accessed < 24 * 3600 && metadata.is_file() {
