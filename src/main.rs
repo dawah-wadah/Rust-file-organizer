eedf1d3e0a86a4095384cf655bf156bfa8bcb59c
Wadah Adlan <wadahadlan@gmail.com>
2022-09-15 09:52:06 -0400

    updated is_path file to accept possible types of endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+#![allow(unused)]
+use std::error::Error;
 use std::{env};
+use std::fs::{self, Metadata};
+
 
 // use walkdir::WalkDir;
 
@@ -7,14 +11,19 @@ fn main() {
 
     let config: Config = Config::new(&args).unwrap();
 
-    println!("Searching for {}", config.query);
-    println!("In file for {}", config.filename);
+    println!("Searching for {}", config.path);
+    println!("In file for {}", config.destination);
+    let path_valid = is_path_redux(&config.path, "directory");
+    let file_valid = is_path_redux(&config.path, "file");
+    let not_valid = is_path_redux(&config.path, "game");
     println!("{:?}", args);
+    println!("Is this filepath leads to a directory: {:?}", path_valid.unwrap());
+    println!("Is this filepath leads to a file: {:?}", file_valid.unwrap());
 }
 
 struct Config {
-    query: String,
-    filename: String
+    path: String,
+    destination: String
 }
 
 impl Config {
@@ -22,14 +31,32 @@ impl Config {
     if args.len() <= 1 {
         return Err("not enough params".to_string())
     }
-        let query: String = args[1].clone();
-        let filename: String = args[2].clone();
+        let path: String = args[1].clone();
+        let destination: String = args[2].clone();
     
-        return Ok(Config{query, filename})
+        return Ok(Config{path, destination})
     }
 
 }
 
+fn is_path_redux(filepath: &str, classifer: &str) -> std::io::Result<bool> {
+ let attr = fs::metadata(filepath)?;
+    // inspect attr ...
+    match classifer {
+        "directory" => return Ok(attr.is_dir()),
+        "file" => return Ok(attr.is_file()),
+        "symlink" =>return Ok(attr.is_symlink()),
+        _ => panic!("not a valid arguement")
+    }
+    // inspect attr ...
+}
+
+fn is_path(filepath: &str) -> std::io::Result<bool> {
+ let attr = fs::metadata(filepath)?;
+    // inspect attr ...
+    Ok(attr.is_dir())
+    // inspect attr ...
+}
 // fn mime(file_path: &str) {
 //     match mime_type::File::new(Path::new(file_path)).get_mime_type() {
 //         Err(e) => {
